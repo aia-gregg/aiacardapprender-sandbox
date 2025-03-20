04aa7ccb9c91f45bfea0766abfac1705a64b19be
aia-gregg <gregg@aianalysis.group>
2025-03-20 19:18:37 +0000

    freezeresponse-fire-v9

--- fireblocks.js
+++ fireblocks.js
@@ -4,7 +4,7 @@ const { FireblocksSDK } = require('fireblocks-sdk');
 
 // Read the private key directly from the .key file.
 // Make sure the file is in the same directory or adjust the path accordingly.
-const PRIVATE_KEY = fs.readFileSync('./fireblocks_secret.key', 'utf8');
+const FIREBLOCKS_PRIVATE_KEY_FILE = fs.readFileSync('./fireblocks_secret.key', 'utf8');
 
 // Get the Fireblocks API key from your environment variables.
 const API_KEY = process.env.FIREBLOCKS_API_KEY;
@@ -13,13 +13,12 @@ const API_KEY = process.env.FIREBLOCKS_API_KEY;
 
 // Set options explicitly, including baseUrl
 const options = { 
-  environment: 'sandbox',
-  baseURL: 'https://sandbox-api.fireblocks.io/v1'
+  environment: 'sandbox'
 };
 
 // Instantiate the Fireblocks SDK client.
 // The SDK will handle JWT signing and other authentication details.
-const fireblocksClient = new FireblocksSDK(PRIVATE_KEY, API_KEY, options);
+const fireblocksClient = new FireblocksSDK(FIREBLOCKS_PRIVATE_KEY_FILE, API_KEY, options);
 
 // Export the functions that wrap the Fireblocks SDK methods.
 module.exports = {
