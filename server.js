fad8d9b2b06ce448d2ba2b792b2988a3f21691aa
aia-gregg <gregg@aianalysis.group>
2025-04-07 21:33:01 +0100

    firebase-token-v3

--- server.js
+++ server.js
@@ -569,15 +569,15 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 // Placeholder functions for sending email and push notifications.
-async function sendTopupEmail(to, subject, body) {
-  console.log(`Sending email to ${to}: ${subject}\n${body}`);
-  // await emailService.send({ to, subject, body });
-}
+// async function sendTopupEmail(to, subject, body) {
+//   console.log(`Sending email to ${to}: ${subject}\n${body}`);
+//   // await emailService.send({ to, subject, body });
+// }
 
-async function sendPushNotification(userId, message) {
-  console.log(`Sending push notification to user ${userId}: ${message}`);
-  // await pushService.send({ userId, message });
-}
+// async function sendPushNotification(userId, message) {
+//   console.log(`Sending push notification to user ${userId}: ${message}`);
+//   // await pushService.send({ userId, message });
+// }
 
 // Webhook endpoint
 app.post(
